53050474436c9bf1ae3cf6b7904a11a178df7848
Tommy Joe Lund <tomlun92@gmail.com>
2018-01-10 22:44:36 +0100

    Update styles
    Change the colour of the grid in the space 'below' the cells.

--- src/components/TicTacToe/Grid.js
+++ src/components/TicTacToe/Grid.js
@@ -3,15 +3,15 @@ import styled from "styled-components";
 import Cell from "./Cell";
 
 const GridStyle = styled.div`
-  background: rgb(131, 79, 44);
-  box-shadow: 0 2px 8px black;
+  background: rgb(65, 40, 23);
+  box-shadow: 0 2px 8px black, inset 0 0 0 25px rgb(131, 79, 44);
   box-sizing: border-box;
   display: grid;
   grid-auto-rows: 1fr;
   grid-template-columns: 1fr 1fr 1fr;
   height: 90%;
   margin: 5%;
-  padding: 5%;
+  padding: 25px;
   width: 90%;
 `;
 

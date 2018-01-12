a360ba7357b0bd6701cf72ec45dff146b936d9ae
Tommy Joe Lund <tomlun92@gmail.com>
2018-01-12 18:37:35 +0100

    Add board state

--- src/components/TicTacToe/index.js
+++ src/components/TicTacToe/index.js
@@ -1,7 +1,19 @@
-import React from "react";
+import React, { Component } from "react";
 import styled from "styled-components";
 import Grid from "./Grid";
 
-const TicTacToe = () => <Grid />;
+class TicTacToe extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      cells: Array(9).fill("")
+    };
+  }
+
+  render() {
+    const { cells } = this.state;
+    return <Grid cells={cells} />;
+  }
+}
 
 export default TicTacToe;
